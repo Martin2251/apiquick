af171819fe6d5611f2fee529b0a2a865b5e99435
Martin Lowe <martinlowe673@gmail.com>
2025-04-08 07:40:13 +0100

    string array

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -35,7 +35,11 @@ app.get('/search',async (c) =>{
     const rank = await redis.zrank("terms", query)
 
     if (rank !== null && rank !== undefined){
-        const temp = await redis.zrange("terms", rank, rank +100)
+        const temp = await redis.zrange<string[]>("terms", rank, rank +100)
+
+        for (const el of temp){
+
+        }
     }
     return c.json({})
 })
